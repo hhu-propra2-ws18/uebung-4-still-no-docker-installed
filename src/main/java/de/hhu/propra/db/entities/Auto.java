fe062abfb6a33460a09230f78143d84a79d34df1
Aydin Seven <aysev100@hhu.de>
2018-11-15 15:35:33 +0000

    bla2

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -2,13 +2,11 @@ package de.hhu.propra.db.entities;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data // Lombok - Generiert Getter, Setter, toString, ...
 @Entity
+@Embeddable
 public class Auto{
     @Id
     @GeneratedValue
